3a7b387ab19a93349c6439158cab5d1be24c6fb6
LEEBONGHAK <dlqhdgkr456@gmail.com>
2023-06-27 22:07:56 +0900

    3.2 Buttons Section

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class App extends StatelessWidget {
       body: Padding(
           padding: EdgeInsets.symmetric(horizontal: 40),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               SizedBox(
                 height: 80,
@@ -26,17 +27,66 @@ class App extends StatelessWidget {
                       Text(
                         'Hey, Selena',
                         style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 28,
-                            fontWeight: FontWeight.w800),
+                          color: Colors.white,
+                          fontSize: 28,
+                          fontWeight: FontWeight.w800,
+                        ),
                       ),
                       Text(
                         'Welcome back',
                         style: TextStyle(
-                            color: Colors.white.withOpacity(0.8), fontSize: 18),
+                          color: Colors.white.withOpacity(0.8),
+                          fontSize: 18,
+                        ),
                       )
                     ],
                   )
+                ],
+              ),
+              SizedBox(
+                height: 120,
+              ),
+              Text(
+                'Total Balance',
+                style: TextStyle(
+                  fontSize: 22,
+                  color: Colors.white.withOpacity(0.8),
+                ),
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              Text(
+                '\$5 194 482',
+                style: TextStyle(
+                  fontSize: 48,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Row(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.amber,
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(
+                        vertical: 20,
+                        horizontal: 50,
+                      ),
+                      child: Text(
+                        'Transfer',
+                        style: TextStyle(
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+                  ),
                 ],
               )
             ],
